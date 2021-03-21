84e6988c4c1c45fa85248d171d64eb3000c71fce
John Bedoya <jovento@gmail.com>
2021-03-21 17:35:19 -0500

    Step_8: Implemented TESTModule method and validation calls.

--- johnbedoya_vog_backend_codechallenge/VogCodeChallenge.ConsoleApp/Program.cs
+++ johnbedoya_vog_backend_codechallenge/VogCodeChallenge.ConsoleApp/Program.cs
@@ -9,10 +9,38 @@ namespace VogCodeChallenge.ConsoleApp
         {
             var namesList = QuestionClass.NamesList;
 
+            Console.WriteLine("Step_7");
+
+            PrintSeparator();
             PrintUsingEnumerator(namesList);
             PrintUsingWhile(namesList);
+
+            PrintSeparator();
+            Console.WriteLine("Step_8");
+            PrintSeparator();
+
+            Console.WriteLine(TESTModule(1));
+            Console.WriteLine(TESTModule(2));
+            Console.WriteLine(TESTModule(3));
+            Console.WriteLine(TESTModule(4));
+            Console.WriteLine(TESTModule(5));
+            Console.WriteLine(TESTModule(1.0f));
+            Console.WriteLine(TESTModule(2.0f));
+            Console.WriteLine(TESTModule("hello world"));
+            Console.WriteLine(TESTModule(new List<string>()));
+
+            try
+            {
+                Console.WriteLine(TESTModule(0));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
         }
 
+        #region Step_7
         private static void PrintSeparator()
         {
             Console.WriteLine("_____________________________________");
@@ -52,5 +80,27 @@ namespace VogCodeChallenge.ConsoleApp
                 enumerator.Dispose();
             }
         }
+        #endregion
+
+        #region Step_8
+        private static object TESTModule(object value)
+        {
+            switch (value)
+            {
+                case int n when (n > 4):
+                    return n * 3;
+                case int n when (n < 1):
+                    throw new ArgumentOutOfRangeException("Value can't be less than 1");
+                case string s:
+                    return s.ToUpper();
+                case int n when (n >= 1 && n <= 4):
+                    return n * 2;
+                case float n when (n >= 1.0f && n <= 2.0f):
+                    return 3.0f;
+                default:
+                    return value;
+            }
+        }
+        #endregion
     }
 }
