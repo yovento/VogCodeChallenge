e7a0e1196dedcc9103d976aae841fb84b08024c0
John Bedoya <jovento@gmail.com>
2021-03-21 16:59:46 -0500

    Step_7: Added new console application project and list iterations methods using while and enumerator.

---
+++ johnbedoya_vog_backend_codechallenge/VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namesList = QuestionClass.NamesList;
+
+            PrintUsingEnumerator(namesList);
+            PrintUsingWhile(namesList);
+        }
+
+        private static void PrintSeparator()
+        {
+            Console.WriteLine("_____________________________________");
+        }
+
+        private static void PrintUsingWhile(List<string> namesList)
+        {
+            PrintSeparator();
+            Console.WriteLine("Printed using while loop");
+            PrintSeparator();
+
+            var counter = 0;
+
+            while (counter < namesList.Count)
+            {
+                Console.WriteLine(namesList[counter]);
+                counter++;
+            }
+        }
+
+        private static void PrintUsingEnumerator(List<string> namesList)
+        {
+            PrintSeparator();
+            Console.WriteLine("Printed using enumerator");
+            PrintSeparator();
+
+            var enumerator = namesList.GetEnumerator();
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current.ToString());
+                }
+            }
+            finally
+            {
+                enumerator.Dispose();
+            }
+        }
+    }
+}
---
+++ johnbedoya_vog_backend_codechallenge/VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+
+}
