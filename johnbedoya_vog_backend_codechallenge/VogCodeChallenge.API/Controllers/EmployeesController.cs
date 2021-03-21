dd2b1eb15f1780d307cc2955eced7b6691bd1705
John Bedoya <jovento@gmail.com>
2021-03-21 15:38:11 -0500

    Step_4: Implemented employee controller and get methods

---
+++ johnbedoya_vog_backend_codechallenge/VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.Domain;
+using VogCodeChallenge.Repositories;
+
+namespace VogCodeChallenge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private IEmployeeRepository _employeeRepository;
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public IList<Employee> GetAll()
+        {   
+            return _employeeRepository.ListAll();
+        }
+
+        [HttpGet]
+        [Route("department/{departmentId}")]
+        public IList<Employee> GetAll(int departmentId)
+        {
+            return _employeeRepository.GetAll().Where(e => e.departmentId == departmentId).ToList();
+        }
+    }
+}
