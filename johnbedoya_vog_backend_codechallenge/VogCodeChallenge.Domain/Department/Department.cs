64e2b911cb04d1a300e24e499070bbd6024b81e7
John Bedoya <jovento@gmail.com>
2021-03-21 13:50:13 -0500

    Step_2: Added base domain objects.

---
+++ johnbedoya_vog_backend_codechallenge/VogCodeChallenge.Domain/Department/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VogCodeChallenge.Domain
+{
+    public class Department : IBaseDomain
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string name { get; set; }
+
+        [StringLength(255)]
+        public string address { get; set; }
+
+    }
+}
---
+++ johnbedoya_vog_backend_codechallenge/VogCodeChallenge.Domain/Employee/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VogCodeChallenge.Domain
+{
+    public class Employee : IBaseDomain
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string firstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string lastName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string jobTitle { get; set; }
+        
+        [MaxLength(100)]
+        public string address { get; set; }
+
+        public Department department { get; set; }
+
+        [Required]
+        public int departmentId { get; set; }
+    }
+}
